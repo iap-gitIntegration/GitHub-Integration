561b56239c58daf7a0b4039f56565cb637863d4e
iap-gitIntegration <iapgithubintegration@gmail.com>
2025-05-13 14:46:05 +0530

    Create QATestFolder/shellscriptwitharguments.sh

---
+++ QATestFolder/shellscriptwitharguments.sh
@@ -0,0 +1,11 @@
+!/bin/bash
+
+#Assign parameters to variables
+string_param=$1
+int_param=$2
+double_param=$3
+
+# Output the values
+echo "String: $string_param"
+echo "Integer: $int_param"
+echo "Double: $double_param"
